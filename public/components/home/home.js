42593c5985b03d0e603a9f3639e12f6b69d57434
Glyn Owen Hanmer <glynhanmer@gmail.com>
2016-10-08 22:02:10 +0100

    InfoCard from WILDLIFE object

--- public/components/home/home.js
+++ public/components/home/home.js
@@ -4,11 +4,30 @@ import Select from 'react-select';
 import CountrySelect from '../countrySelect/countrySelect';
 import InfoCard from '../infoCard/infoCard';
 
-var WILDLIFE = ["foo", "bar", "eep"];
+var WILDLIFE = [
+  {
+    name:"Panda",
+    conservation_status:"bad ass",
+    description:"Pretty bad ass bear",
+    animal_image:"https://upload.wikimedia.org/wikipedia/commons/d/d5/Panda_Mark.svg"
+  },
+  {
+    name:"Pig",
+    conservation_status:"bad ass",
+    description:"Pretty bad ass bear",
+    animal_image:"http://www.beliefnet.com/columnists//wholeliving/files/2011/08/tinypig.jpg"
+  },
+  {
+    name:"Panda",
+    conservation_status:"bad ass",
+    description:"Pretty bad ass bear",
+    animal_image:"http://www.eonline.com/eol_images/Entire_Site/2013229/rs_560x415-130329112005-1024.BabyPig1.mh.032913.jpg"
+  }
+];
 var rows = [];
 
 for (var i=0; i < WILDLIFE.length; i++) {
-    rows.push(<InfoCard text={WILDLIFE[i]} name={WILDLIFE[i]} conservation_status={"NC"}/>);
+  rows.push(<InfoCard description={WILDLIFE[i].description} name={WILDLIFE[i].name} conservation_status={WILDLIFE[i].conservation_status} animal_image={WILDLIFE[i].animal_image}/>);
 }
 
 export default class Home extends React.Component {
--- public/components/infoCard/infoCard.js
+++ public/components/infoCard/infoCard.js
@@ -5,11 +5,10 @@ export default class InfoCard extends React.Component {
   render() {
     return(
       <div className="infoCard">
-        <h2 className="infoCard__header">ANIMAL NAME</h2>
+        <h2 className="infoCard__header">{this.props.name}</h2>
         <div className="infoCard__images">
             <img className="infoCard__image"
-            // style={{width: 50, height: 50}}
-            src={'https://upload.wikimedia.org/wikipedia/commons/d/d5/Panda_Mark.svg'} />
+            src={this.props.animal_image} />
             <img className="infoCard__productImage"
             src={'https://s-media-cache-ak0.pinimg.com/736x/44/91/31/449131cabd0f6c51259cf881355368a1.jpg'} />
             <img className="infoCard__productImage"
@@ -19,8 +18,8 @@ export default class InfoCard extends React.Component {
             <img className="infoCard__productImage"
             src={'https://s-media-cache-ak0.pinimg.com/736x/44/91/31/449131cabd0f6c51259cf881355368a1.jpg'} />
         </div>
-        <div className="infoCard__conservation_status">Conservation Status: {this.props.conservation_status}</div>
-        <div className="infoCard__text">{this.props.text}</div>
+        <div className="infoCard__conservation_status">Conservation Status: <b>{this.props.conservation_status}</b></div>
+        <div className="infoCard__text">{this.props.description}</div>
       </div>
     )
   }

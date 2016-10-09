1594f23255ca152b8f1dec3bfa56b453d41ffd79
Glyn Owen Hanmer <glynhanmer@gmail.com>
2016-10-09 10:35:51 +0100

    BROKEN IMAGES

--- public/components/home/home.js
+++ public/components/home/home.js
@@ -7,23 +7,26 @@ import InfoCard from '../infoCard/infoCard';
 var wildlifeData = require( "../../data.json" );
 var animalConservationStatus = require("../../animals_with_status.json");
 
-var cam = wildlifeData.Namibia;
-for (var item in cam) {
-  var rows = [];
+var country = wildlifeData.Namibia;
+var rows = [];
+for (var item in country) {
+  var animal = country[item]; 
   var popular_items;
   var confiscatedItems = [];
-  for (var i = cam[item].items.length - 1; i >= 0; i--) {
-    confiscatedItems.push(cam[item].items[i][0]);
+  for (var i = animal.items.length - 1; i >= 0; i--) {
+    confiscatedItems.push(animal.items[i].item);
   }
   popular_items = confiscatedItems.join(",  ");
-
   rows.push(<InfoCard
     popular_items={popular_items}
-    name={item}
-    conservation_status={animalConservationStatus[item]}
+    name={animal.name}
+    conservation_status={animal.cons}
     animal_image={"http://www.eonline.com/eol_images/Entire_Site/2013229/rs_560x415-130329112005-1024.BabyPig1.mh.032913.jpg"}
     product_image={"http://www.metmuseum.org/-/media/Images/Press/Press%20Images/Exhibitions/2011/Game%20of%20Kings/Teaser_King-front.jpg"} />);
 }
+
+console.log("rows");
+console.log(rows);
 
 export default class Home extends React.Component {
   constructor(props) {
